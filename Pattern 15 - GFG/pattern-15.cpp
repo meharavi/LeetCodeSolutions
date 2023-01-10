2253f01e0f6901b204683a9748c1ce88d1dedc33
Meha Ravi <37682502+meharavi@users.noreply.github.com>
2023-01-10 21:32:27 +0530

    Added solution - LeetHub

---
+++ Pattern 15 - GFG/pattern-15.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+       for(int i=n;i>0;i--){
+            for(int j=1;j<=i;j++){
+                cout<<char(j+64);
+            }
+            cout<<"\n";
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
